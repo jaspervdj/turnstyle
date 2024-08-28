ac253d2b319341d58468c917d973485c1cf9605a
Jasper Van der Jeugt <m@jaspervdj.be>
2024-08-28 22:03:56 +0200

    Add compile test using import

--- hs/test/Turnstyle/Compile/Tests.hs
+++ hs/test/Turnstyle/Compile/Tests.hs
@@ -2,7 +2,9 @@ module Turnstyle.Compile.Tests
     ( tests
     ) where
 
+import qualified Codec.Picture          as JP
 import           Data.Either.Validation (Validation (..))
+import qualified Data.Map               as M
 import           Test.Tasty             (TestTree, testGroup)
 import           Test.Tasty.HUnit       (testCase, (@?=))
 import qualified Test.Tasty.QuickCheck  as QC
@@ -13,6 +15,7 @@ import           Turnstyle.Eval.Tests   (EvalState (..), emptyEvalState,
                                          runEvalPure)
 import           Turnstyle.Expr
 import           Turnstyle.Expr.Tests
+import           Turnstyle.Image
 import           Turnstyle.JuicyPixels
 import           Turnstyle.Parse
 import           Turnstyle.Text         (exprToSugar, parseSugar)
@@ -35,7 +38,9 @@ tests = testGroup "Turnstyle.Compile"
                 Success parsed -> toDeBruijn expr == toDeBruijn parsed
     , testCase "rot13" $ do
         sugar <- either (fail . show) pure $ parseSugar "rot13.txt" rot13
-        img <- either (fail . show) pure $ compile defaultCompileOptions sugar
+        img <- either (fail . show) pure $ compile
+            defaultCompileOptions {coImports = M.singleton "y.png" yImage}
+            sugar
         let expr = parseImage Nothing (JuicyPixels img)
             (result, finalState) = runEvalPure (eval expr)
                 emptyEvalState {esInChars = "abc\ndef\n"}
@@ -44,7 +49,7 @@ tests = testGroup "Turnstyle.Compile"
     ]
   where
     rot13 = unlines
-        [ "LET y = λf. (λx. f (x x)) (λx. f (x x)) IN"
+        [ "LET y = IMPORT \"y.png\" IN"
         , "LET char_a = num_add (num_mul 10 9) 7 IN"
         , "LET char_z = num_add char_a 25 IN"
         , "LET and = λp q. p q p IN"
@@ -55,3 +60,26 @@ tests = testGroup "Turnstyle.Compile"
         , "        n IN"
         , "y (λrec. in_char (λn. out_char (rot13 n) rec) (num_sub 1 1))"
         ]
+
+yImage :: JuicyPixels
+yImage = mkJuicyPixels
+    [ [a, a, a, a, y, a, a]
+    , [a, a, g, y, b, y, a]
+    , [a, r, g, b, g, b, y]
+    , [y, y, g, b, r, y, a]
+    , [r, r, r, r, y, a, a]
+    , [y, y, a, b, g, a, a]
+    , [y, b, g, b, g, r, a]
+    , [a, y, b, y, g, a, a]
+    , [a, a, y, a, a, a, a]
+    ]
+  where
+    a = JP.PixelRGBA8   0   0   0   0
+    y = JP.PixelRGBA8 255   0   0 255
+    g = JP.PixelRGBA8 255 255   0 255
+    b = JP.PixelRGBA8   0 255 255 255
+    r = JP.PixelRGBA8   0   0 255 255
+
+mkJuicyPixels :: [[Pixel JuicyPixels]] -> JuicyPixels
+mkJuicyPixels pixels = JuicyPixels $ JP.generateImage
+    (\x y -> pixels !! y !! x) (length (head pixels)) (length pixels)
